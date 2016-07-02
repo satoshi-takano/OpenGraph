26648b4c4cfa6149ceb3d402891c76c88a9ba824
satoshi-takano <satoshi.takano@dena.com>
2016-07-02 17:06:22 +0900

    Initial commit

---
+++ OpenGraph/OpenGraph.h
@@ -0,0 +1,19 @@
+//
+//  OpenGraph.h
+//  OpenGraph
+//
+//  Created by Satoshi Takano on 2016/07/02.
+//  Copyright © 2016年 Satoshi Takano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenGraph.
+FOUNDATION_EXPORT double OpenGraphVersionNumber;
+
+//! Project version string for OpenGraph.
+FOUNDATION_EXPORT const unsigned char OpenGraphVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenGraph/PublicHeader.h>
+
+
