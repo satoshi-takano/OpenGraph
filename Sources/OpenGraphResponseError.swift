5c679cb9323f957e280340e4aedbaf59662fbead
satoshi-takano <satoshi.takano@dena.com>
2016-07-02 19:09:58 +0900

    Implement web page fetching method

---
+++ Sources/OpenGraphResponseError.swift
@@ -0,0 +1,13 @@
+//
+//  OpenGraphResponseError.swift
+//  OpenGraph
+//
+//  Created by Satoshi Takano on 2016/07/02.
+//  Copyright © 2016年 Satoshi Takano. All rights reserved.
+//
+
+import Foundation
+
+public enum OpenGraphResponseError: ErrorType {
+    case UnexpectedStatusCode(Int)
+}
