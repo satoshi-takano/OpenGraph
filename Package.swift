599c5a49971949c4091ed756df525ab3ad7ae5d1
Satoshi Takano <satoxi.tacano@gmail.com>
2021-01-11 13:02:32 +0900

    Fix package resolving issue

--- Package.swift
+++ Package.swift
@@ -3,5 +3,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "OpenGraph"
+    name: "OpenGraph",
+    platforms: [
+            .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+    ],
+    products: [
+        .library(name: "OpenGraph", targets: ["OpenGraph"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "OpenGraph",
+            dependencies: []
+        ),
+         .testTarget(
+            name: "OpenGraphTests",
+            dependencies: ["OpenGraph"]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
 )
