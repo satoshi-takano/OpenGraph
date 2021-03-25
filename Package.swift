3eb757da12b831b9e4a09d44d2ff7a39d2a0d510
Satoshi Takano <satoxi.tacano@gmail.com>
2021-03-25 20:07:10 +0900

    Specify path

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     targets: [
         .target(
             name: "OpenGraph",
+            path: "Sources/OpenGraph",
             dependencies: []
         ),
          .testTarget(
