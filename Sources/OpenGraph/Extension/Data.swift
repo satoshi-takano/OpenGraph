be1b3408eae4665234aea449518a8027b49ecabe
Soroush Khanlou <soroushkhanlou@gmail.com>
2022-07-04 12:19:40 -0400

    fix bug

--- Sources/OpenGraph/Extension/Data.swift
+++ Sources/OpenGraph/Extension/Data.swift
@@ -12,11 +12,11 @@ extension Data {
     @available(macOS 10.10, *)
     var stringEncoding: String.Encoding? {
 #if os(Linux)
+        return nil
+#else
         var nsString: NSString?
         guard case let rawValue = NSString.stringEncoding(for: self, encodingOptions: nil, convertedString: &nsString, usedLossyConversion: nil), rawValue != 0 else { return nil }
         return String.Encoding(rawValue: rawValue)
-#else
-        return nil
 #endif
     }
 }
